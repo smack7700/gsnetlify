2374a6f4ad6405c528bc46b962f4e51ec33b2a57
smack7700 <154453630+smack7700@users.noreply.github.com>
2023-12-23 22:20:36 +0500

    Add files via upload

---
+++ Contact form to email.js
@@ -0,0 +1,28 @@
+function emailSend(){
+
+	var userName = document.getElementById('name').value;
+	var phone = document.getElementById('phone').value;
+	var email = document.getElementById('email').value;
+
+	var messageBody = "Name " + userName +
+	"<br/> Phone " + phone +
+	"<br/> Email " + email;
+	Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "smackking7002@gmail.com",
+    Password : "2EA593DBCEE6D413C5E42C9F65E58D9A16FA",
+    To : 'smack7700@gmail.com',
+    From : "smack7002@gmail.com",
+    Subject : "This is the subject",
+    Body : messageBody
+}).then(
+  message => {
+  	if(message=='OK'){
+  		swal("Secussful", "You clicked the button!", "success");
+  	}
+  	else{
+  		swal("Error", "You clicked the button!", "error");
+  	}
+  }
+);
+}
